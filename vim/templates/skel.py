ca0878111a612085ec2588f4ae5abec8c52df14f
davemachado <dave@davemachado.com>
2017-11-20 17:36:02 -0500

    Add new templates and update old

--- vim/templates/skel.py
+++ vim/templates/skel.py
@@ -1,1 +1,16 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
+# Sohit Pal
+
+def main():
+    MARKER
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt, e: # Ctrl-C
+        raise e
+    except Exception, e:
+        print 'ERROR, UNEXPECTED EXCEPTION'
+        print str(e)
+        traceback.print_exc()
+        os._exit(1)
--- vim/templates/skel.sh
+++ vim/templates/skel.sh
@@ -1,1 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/sh
+
+if [ "$#" -eq 0 ]; then
+	# NO ARGUMENTS GIVEN
+    MARKER
+else
+	# ATLEAST 1 ARGUMENT GIVEN starting in $1
+
+fi
