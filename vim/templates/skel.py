11ee386038a008320b2cb2d96248063c50c51a9b
davemachado <dave@davemachado.com>
2017-11-27 17:49:40 -0500

    Update templates to remove old name

--- vim/templates/skel.py
+++ vim/templates/skel.py
@@ -1,16 +1,7 @@
-#!/usr/bin/env python
-# Sohit Pal
+#!/usr/bin/env python3
 
 def main():
-    MARKER
+    print "Dave"
 
 if __name__ == "__main__":
-    try:
-        main()
-    except KeyboardInterrupt, e: # Ctrl-C
-        raise e
-    except Exception, e:
-        print 'ERROR, UNEXPECTED EXCEPTION'
-        print str(e)
-        traceback.print_exc()
-        os._exit(1)
+    main()
